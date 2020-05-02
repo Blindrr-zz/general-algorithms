628169a0ddb53865910684d4c7d759e5f78aede8
Matheus Henrique Mello Rebouças <48971358+Blindrr@users.noreply.github.com>
2020-05-01 21:50:50 -0300

    Create baladovski.c

---
+++ baladovski.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <windows.h>
+
+void main() {
+
+    while(1 == 1) {
+
+        system("color 01");
+        system("color 12");
+        system("color 21");
+        system("color 41");
+        system("color 51");
+        system("color 61");
+        system("color 71");
+        system("color 81");
+        system("color 91");
+        system("color a1");
+        system("color b1");
+        system("color f1");
+        system("color c1");
+        system("color d1");
+    }
+}
